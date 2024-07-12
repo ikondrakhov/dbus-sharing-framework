47008a4315f91ecb3be3360b91916355c9061278
Igor Kondrakhov <kondrahov.2000@mail.ru>
2024-07-12 14:30:56 +0300

    Fixed GetFileFormat method

--- server/ShareService.cpp
+++ server/ShareService.cpp
@@ -1,20 +1,27 @@
 #include "ShareService.hpp"
 
 #include <QtDBus/QtDBus>
+#include <iostream>
 
 ShareService::ShareService() {}
 
 QString ShareService::RegisterService(QString name,
                                       QStringList supportedFormats) {
+  std::cout << "Formats:" << std::endl;
   for (const QString &format : supportedFormats) {
     this->formatToService[format].push_back(name);
+    std::cout << format.toStdString() << std::endl;
   }
   this->serviceToFormats[name] = supportedFormats;
+  std::cout << "Registered service with name: " << name.toStdString()
+            << std::endl;
   return "register service";
 }
 
 QString ShareService::OpenFile(QString path) {
-  const auto &services = this->formatToService[this->GetFileFormat(path)];
+  QString fileFormat = this->GetFileFormat(path);
+  std::cout << fileFormat.toStdString() << std::endl;
+  const auto &services = this->formatToService[fileFormat];
   if (services.size() == 0) {
     return "Error: No service to open file with format";
   }
@@ -27,12 +34,16 @@ QString ShareService::OpenFileUsingService(QString path, QString service) {
   for (const QString &format : this->serviceToFormats[service]) {
     if (format == fileFormat) {
       QDBusReply<QString> response = serviceInterface.call("OpenFile", path);
-      return "open file using service";
+      if (response.isValid()) {
+        return "Ok";
+      } else {
+        return QString("Error opening file with service: ") + service;
+      }
     }
   }
   return "Error opening file with service";
 }
 
 QString ShareService::GetFileFormat(QString path) {
-  return path.right(path.lastIndexOf('.'));
+  return path.right(path.length() - path.lastIndexOf('.') - 1);
 }
